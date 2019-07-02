ccbf59e7558efd1c60e83a56471117c70fc54d0c
Paunczki <johnnymikos@gmail.com>
2019-07-02 10:25:22 -0500

    Added ways to have reference changed automatically

--- src/HashCount.java
+++ src/HashCount.java
@@ -2,8 +2,29 @@ import java.util.*;
 import java.io.*;
 public class HashCount {
 	static HashMap<Double, Integer> map = new HashMap<Double, Integer>();
+
+	public HashCount(File dirForReference) throws FileNotFoundException {
+		run(dirForReference);
+	}
+
 	public static void main(String[] args) throws FileNotFoundException{
-		long time1 = System.currentTimeMillis();
+		run();
+	}
+
+	public static void run(File dir) throws FileNotFoundException {
+		// long time1 = System.currentTimeMillis();
+		int counter = 0;
+		for (File fileA : dir.listFiles()) {
+			countThem(fileA);
+			System.out.println(counter++);
+		}
+		getKeys();
+		// long time2 = System.currentTimeMillis();
+		// System.out.println("\n\nTime taken: " + ((time2-time1)/1000) + " seconds");
+	}
+
+	public static void run() throws FileNotFoundException {
+		// long time1 = System.currentTimeMillis();
 		File dir = new File("result");
 		int counter = 0;
 		for (File fileA : dir.listFiles()) {
@@ -11,8 +32,8 @@ public class HashCount {
 			System.out.println(counter++);
 		}
 		getKeys();
-		long time2 = System.currentTimeMillis();
-		System.out.println("\n\nTime taken: " + ((time2-time1)/1000) + " seconds");
+		// long time2 = System.currentTimeMillis();
+		// System.out.println("\n\nTime taken: " + ((time2-time1)/1000) + " seconds");
 	}
 	public static void writeReference(String input) {
 		BufferedWriter bf;
@@ -34,23 +55,23 @@ public class HashCount {
 		while(scanner.hasNextLine()) {
 			// make sure to skip the first two instances since its name 0 ...
 			try {
-			String data = scanner.nextLine();
-			String[] splits = data.split(" ");
-			String[] time1 = splits[2].split(":");
-			Double timeInitial = Double.parseDouble(time1[0]);
-			for(int i=2; i<splits.length; i++) {
-				String[] splits2 = splits[i].split(":");
+				String data = scanner.nextLine();
+				String[] splits = data.split(" ");
+				String[] time1 = splits[2].split(":");
+				Double timeInitial = Double.parseDouble(time1[0]);
+				for(int i=2; i<splits.length; i++) {
+					String[] splits2 = splits[i].split(":");
 //				if(Double.parseDouble(splits2[0]) > (timeInitial+20.0)) {
 //					break;
 //				}
-				if(!map.containsKey(Double.parseDouble(splits2[1]))) {
-					map.put(Double.parseDouble(splits2[1]), 1);
+					if(!map.containsKey(Double.parseDouble(splits2[1]))) {
+						map.put(Double.parseDouble(splits2[1]), 1);
+					}
+					else {
+						int temp = map.get(Double.parseDouble(splits2[1]));
+						map.put(Double.parseDouble(splits2[1]), temp+1);
+					}
 				}
-				else {
-					int temp = map.get(Double.parseDouble(splits2[1]));
-					map.put(Double.parseDouble(splits2[1]), temp+1);
-				}
-			}
 			}
 			catch(Exception e) {
 				continue;
